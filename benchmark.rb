2d3e0bee22b5e6846b8e15bf3ab1bfc49b8a00cb
John Bachir <j@jjb.cc>
2023-07-09 09:40:15 -0400

    DRY

--- benchmark.rb
+++ benchmark.rb
@@ -5,25 +5,24 @@ require 'benchmark/ips'
 require_relative 'time_limit.rb'
 require 'timeout'
 
+
+def the_code(the_class)
+  threads = []
+  100.times do
+    threads << Thread.new do
+      value = the_class.timeout(1) {:ok}
+    end
+  end
+  threads.each(&:join)
+end
+
 Benchmark.ips do |x|
   x.report("TimeLimit") do
-    threads = []
-    100.times do
-      threads << Thread.new do
-        value = TimeLimit.timeout(1) {:ok}
-      end
-    end
-    threads.each(&:join)
+    the_code(TimeLimit)
   end
 
   x.report("Timeout") do
-    threads = []
-    100.times do
-      threads << Thread.new do
-        value = Timeout.timeout(1) {:ok}
-      end
-    end
-    threads.each(&:join)
+    the_code(Timeout)
   end
 
   x.compare!
